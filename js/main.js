536a3fca4e28ba1ca6cb568317c112f0d12b8d44
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2018-11-02 18:45:04 -0400

    First comit
    Changed Font, added import
    started script

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ Author: @
 Description: 
 Date: 
 *********************************/
+document.addEventListener("DOMContentLoaded", init);
+
+let url ; //to be filled in 
+    
+let req = new Request(url, {
+    method:"POST",
+    body:"form"
+});
+
+let pages = [];
+
+
+function init(){
+    pages = document.querySelectorAll(".page");
+    console.log(pages);
+    
+    
+    
+//  trying to organize pages
+    
+//    document.getElementById("btnSend").addEventListener("click", function (){
+//            pages[0].target.toggleClass("show");
+//            pages[1].target.toggleClass("show");
+//        
+//        });
+//    document.getElementById("btnBack").addEventListener("click", function(){
+//             pages[0].target.toggleClass("show");
+//             pages[1].target.toggleClass("show");
+//    });
+}
+    //let pages = [];
+    //
+    //function init () {
+    //    pages = document.querySelectorAll(".page");
+    //    console.log(pages);
+    //}
