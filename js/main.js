86caefa7baf1d5be62cece3431d55a82129b6e38
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2018-11-07 21:20:32 -0500

    made script

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", init);
 let uri = "https://davidst.edumedia.ca/mad9014/nums.php";
+let pages = [];
 
 function init() {
-    document.getElementById("btnSend").addEventListener("click", getData);
 
+    document.getElementById("btnSend").addEventListener("click", getData); //sends data
+
+    pages = document.querySelectorAll(".page")
+    console.log(pages);
+    document.getElementById("btnBack").addEventListener("click", function () {
+        pages[0].classList.toggle("hidden");
+        pages[1].classList.toggle("hidden");
+    });
+
+    document.getElementById("btnSend").addEventListener("click", function () {
+        pages[0].classList.toggle("hidden");
+        pages[1].classList.toggle("hidden");
+    });
 }
-
+/////////////////////////////////////////
+///function to communicate with server///
+/////////////////////////////////////////
 function getData() {
 
     let formdata = new FormData();
@@ -21,10 +36,40 @@ function getData() {
             console.log(response);
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
+        .then(function (jsonData) {
+            console.log(jsonData);
+            let array = jsonData.numbers
+            console.log(array[0]);
+            
+            
+            let ul = document.getElementById("num_list");
+            let df = new DocumentFragment();
+        for (let i = 0; i <array.length; i++) {
+            let li = document.getElementById("li");
+                li.textContent = array[i];
+                li.className = "page";
+                df.appendChild(li)
+        }
+//            jsonData.numbers.forEach(function (number) {
+//                let li = document.getElementById("li");
+//                li.textContent = array[i];
+//                li.className = "page";
+//                df.appendChild(li)
+//            });
+            ul.appendChild(df);
         })
         .catch(function (err) {
-            alert("error:" + err.message);
+            alert("error: " + err.message);
         })
+}
+//////////////////////////////
+///function to change pages///
+//////////////////////////////
+let pageChange = function (pages) {
+    for (let i = 0; i < pages.length; i++)
+        if (pages == i) {
+            pages[i].addClass("hidden");
+        } else {
+            pages[i].removeClass("hidden");
+        }
 }
